83ac9cf319cc30a8cdd0179e2761f0d34065bbbc
Sparsh Rawlani <sparshrawlani77@gmail.com>
2020-01-06 12:44:02 -0800

    Added both the hw's

---
+++ Windows_Apps/Project1/Project1/Project1_EX_01.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Enter Username");
+		
+		string userName = Console.ReadLine();
+		Console.WriteLine();
+
+		Console.WriteLine("I see your name to the system is called as " + userName);
+		Console.WriteLine();
+
+		Console.WriteLine("What do you preferred to be called ?");
+		string prefName = Console.ReadLine();
+		Console.WriteLine();
+		Console.WriteLine("Hi " + prefName + " ,How old are you?");
+		int age = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine();
+		Console.WriteLine(prefName + "'s age is " + age + " years!");
+		Console.WriteLine();
+
+		if (age <= 17)
+		{
+			Console.BackgroundColor = ConsoleColor.Red;
+			Console.WriteLine("Hello" + prefName + ", You are a minor!", Console.BackgroundColor);
+			Console.Beep();
+		}
+		else
+		{
+			Console.BackgroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("Hello "+ prefName + ", You are not a minor!", Console.BackgroundColor);
+			MessageBox.Show("!!!!DANGER!!!! " + prefName);
+		}
+
+	}
+}
---
+++ Windows_Apps/Project1_EX_02/Project1_EX_02/Project_EX_01.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public class Calculator
+{
+
+	public int Addition(int input1, int input2)
+	{
+		return input1 + input2;
+	}
+
+	public int Subtraction(int input1, int input2)
+	{
+		return input1 - input2;
+	}
+
+	public int Multiplication(int input1, int input2)
+	{
+		return input1 * input2;
+	}
+
+	public int Division(int input1, int input2)
+	{
+		return input1 / input2;
+	}
+}
+
+public class Class1
+{
+	static public void Main(string[] args)
+	{
+		Calculator cal = new Calculator();
+		Console.WriteLine("Welcome to a simple Calculator in C#");
+		Console.WriteLine();
+		char exit;
+
+		do
+		{
+			Console.WriteLine("What operation would you like to perform?");
+			Console.WriteLine("Press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division");
+			int input = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("You entered " + input);
+
+
+			if (input == 1)
+			{
+				Console.WriteLine("Enter the two numbers");
+				int num1 = Convert.ToInt32(Console.ReadLine());
+				int num2 = Convert.ToInt32(Console.ReadLine());
+				MessageBox.Show("The sum of two numbers is " + cal.Addition(num1, num2));
+			}
+
+			else if (input == 2)
+			{
+				Console.WriteLine("Enter the two numbers");
+				int num1 = Convert.ToInt32(Console.ReadLine());
+				int num2 = Convert.ToInt32(Console.ReadLine());
+				MessageBox.Show("The difference of two numbers is " + cal.Subtraction(num1, num2));
+			}
+
+			else if (input == 3)
+			{
+				Console.WriteLine("Enter the two numbers");
+				int num1 = Convert.ToInt32(Console.ReadLine());
+				int num2 = Convert.ToInt32(Console.ReadLine());
+				MessageBox.Show("The product of two numbers is " + cal.Multiplication(num1, num2));
+			}
+
+			else if (input == 4)
+			{
+				Console.WriteLine("Enter the two numbers");
+				int num1 = Convert.ToInt32(Console.ReadLine());
+				int num2 = Convert.ToInt32(Console.ReadLine());
+				MessageBox.Show("The division of two numbers is " + cal.Division(num1, num2));
+			}
+			Console.WriteLine();
+			Console.WriteLine("Would you like to restart?");
+			exit = Convert.ToChar(Console.ReadLine());
+		} while (exit == 'y' || exit == 'Y');
+
+	}
+}
